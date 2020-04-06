50b762775ed16952abec93a049a6662bc4964c1e
Joseph <joseph.wambo@kps-it.com>
2020-04-06 23:12:35 +0200

    Modification de la méthode  ajouterProduit afin de vérifier que le prix de vente n’est pas égal à 0.

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -69,11 +70,15 @@ public class ProductController {
     @PostMapping(value = "/Produits")
 
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
+    	
+    	if (product.getPrix() == 0)
+        	throw new ProduitGratuitException("Le produit ne peut être enregistrer car le prix doit être différent de 0. Rien n'est gratuit!");
 
         Product productAdded =  productDao.save(product);
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
+        
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
